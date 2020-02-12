0ae4bc23944be1213c4a5a559eff2f28bc50adc7
Kris <kriskuiper74@gmail.com>
2020-02-12 21:20:06 +0100

    feat: set up mental model for the library

---
+++ index.js
@@ -0,0 +1,19 @@
+import getHandlers from './lib/get-handlers'
+
+/**
+ * @description Syncs an object literal to sessionStorage
+ * @param {string} name Name of the value in sessionStorage
+ * @param {*} value Value that should be synced with sessionStorage
+ */
+export const syncWithSessionStorage = (name, value) => {
+    return new Proxy(value, getHandlers('sessionStorage', name))
+}
+
+/**
+ * @description Syncs an object literal to localStorage
+ * @param {string} name Name of the value in localStorage
+ * @param {*} value Value that should be synced with localStorage
+ */
+export const syncWithLocalStorage = (name, value) => {
+    return new Proxy(value, getHandlers('localStorage', name))
+}
