85274ce0ee79b1cff076fab8995fff8bdf13e177
Kris <kriskuiper74@gmail.com>
2020-03-11 15:30:42 +0100

    test: write test for index page

--- test/index.test.js
+++ test/index.test.js
@@ -1,17 +1,51 @@
+const {
+    syncWithLocalStorage,
+    syncWithSessionStorage
+} = require('../src/index.js')
+
+const user = {
+    name: 'Henk',
+    surname: 'Surname'
+}
+
 describe('Syncs with localStorage', () => {
     test('Inits item in localStorage', () => {
-        return Promise.resolve()
-    })
+        const storage = window.localStorage
+        const person = syncWithLocalStorage('person', user)
+        const item = JSON.parse(storage.getItem('person'))
 
-    test('Inits item in sessionStorage', () => {
-        return Promise.resolve()
+        expect(item).toEqual(person)
     })
 
     test('Updates item in localStorage', () => {
-        return Promise.resolve()
+        const storage = window.localStorage
+        const person = syncWithLocalStorage('person', user)
+
+        person.name = 'Test'
+
+        const item = JSON.parse(storage.getItem('person'))
+
+        expect(item).toEqual(person)
+    })
+})
+
+describe('Syncs with sessionStorage', () => {
+    test('Inits item in sessionStorage', () => {
+        const storage = window.sessionStorage
+        const person = syncWithSessionStorage('person', user)
+        const item = JSON.parse(storage.getItem('person'))
+
+        expect(item).toEqual(person)
     })
 
     test('Updates item in sessionStorage', () => {
-        return Promise.resolve()
+        const storage = window.sessionStorage
+        const person = syncWithSessionStorage('person', user)
+
+        person.name = 'Test'
+
+        const item = JSON.parse(storage.getItem('person'))
+
+        expect(item).toEqual(person)
     })
 })
