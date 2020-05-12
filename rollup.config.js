a8833e044d24b32fa3e6a28ecb751b5a8d838971
Kris <kriskuiper74@gmail.com>
2020-05-13 00:24:18 +0200

    chore: build to cjs format

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,14 @@
+import commonjs from '@rollup/plugin-commonjs'
+import resolve from '@rollup/plugin-node-resolve'
+
 module.exports = {
     input: 'src/index.js',
     output: {
-        file: 'index.js'
+        file: 'index.js',
+        format: 'cjs'
     },
+    plugins: [
+        commonjs(),
+        resolve()
+    ]
 }
